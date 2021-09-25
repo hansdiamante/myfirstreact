ca0f7b777ff8971df6f7e5bf3b82cb77ae9d575d
hansdiamante <90952311+hansdiamante@users.noreply.github.com>
2021-09-25 18:39:44 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"https://www.seekpng.com/png/full/172-1729819_every-spread-of-the-2013-2014-marshall-yearbook.png"} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. ---carlos diff
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
