49028c24daad6140abd632b909c710667e82dbe3
hansdiamante <hans.diamante.cics@ust.edu.ph>
2021-12-15 20:55:39 +0800

    update

--- src/components/Router.js
+++ src/components/Router.js
@@ -10,10 +10,10 @@ const Router = () =>{
                 <BrowserRouter>
                     <Routes>
                         <Route exact path ="/" element={<Home/>}/>
-                        <Route exact path ="/myfirstreact" element={<Home/>}/>
-                        <Route exact path ="/employees" element={<Employee/>}/>
-                        <Route exact path ="/add" element={<AddEmployee/>}/>
-                        <Route exact path ="/edit/:employeeId" element={<AddEmployee/>}/>
+                        <Route exact path ="/fdasfasfd" element={<Home/>}/>
+                        <Route exact path ="/fdasfasfd/employees" element={<Employee/>}/>
+                        <Route exact path ="/fdasfasfd/add" element={<AddEmployee/>}/>
+                        <Route exact path ="/fdasfasfd/edit/:employeeId" element={<AddEmployee/>}/>
                         <Route exact path ="*" element={<PageNotFound/>}/>
                     </Routes>
                 </BrowserRouter>
